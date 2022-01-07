703b4556ef8942e3d9c779b4215f4c2965d36cdb
amartinez808 <amartinez@email.neit.edu>
2022-01-07 15:06:31 -0500

    "boooooo"

--- se-251/index.js
+++ se-251/index.js
@@ -1,3 +1,28 @@
+/*
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
 
-    console.log('hello world');
+let name = "Tony";      //String literal
+let age = 33;           //Number literal
+let isApproved = false; //Boolean literal
+let firstName = undefined;
+let selectedColor = null;
+*/
+
+//performing a task
+function greet(name, lastname){
+    console.log('Hello'+name+''+lastname)
+}
+greet('Joe','Doe');
+greet('Lady');
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
+
+
+
  
