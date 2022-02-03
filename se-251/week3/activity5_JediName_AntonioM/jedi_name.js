ec72ebc82db4e007c900a5e23b4c9a30f633d023
amartinez808 <amartinez@email.neit.edu>
2022-02-03 09:03:02 -0500

    validation assignment.

--- se-251/week3/activity5_JediName_AntonioM/jedi_name.js
+++ se-251/week3/activity5_JediName_AntonioM/jedi_name.js
@@ -23,70 +23,84 @@ const joinList = evt => {
     let hourly = $("#hourly").value;
     let hours = $("#hours").value;
     let tax = $("#tax").value;
-    let tSeason = $("#season").value;
-    let tSmurf = $("#smurf").value;
-
+    let name = $("#name").value;
+    
+    alert("Full Name: " + name);
 
     //email_1  => stuff before @ => Find the @(indexOf), copy the stuff from 0 to @ (exlusive)
-    let tPos1 = tEmail.indexOf("@", 0);
+    let grosspay = hourly * hours;
+    alert("Grosspay: " + grosspay);
 
-    tEmail = tEmail.substr(0, tPos1);
 
-    alert("Email: " + tEmail);
+    let tTax = grosspay * tax;
+    alert("Taxes: " + tTax);
+
+
+    let netpay = grosspay - tTax;
+
+    alert("netpay: " + netpay);
+
 
 
 //first_name => last 2 characters of the name => Get length (1 greater than last subscript) -> startingPoint = length -2, Copy (substr) 2 chars
-    let lenFirst = tFirstName.length;
-    tFirstName = tFirstName.substr(lenFirst - 2, 2);
+   // let lenFirst = tFirstName.length;
+    //tFirstName = tFirstName.substr(lenFirst - 2, 2);
 
-    alert(tFirstName);
+    //alert(tFirstName);
 
     //smurf / personality trait => copy the first three characters => substr(0, 3)
-    tSmurf = tSmurf.substr(0, 3);
+    //tSmurf = tSmurf.substr(0, 3);
 
 
 // season => => Does it have an A, U, I || If it has an A, Store an A  || Else if has an I, Store it  else Store the u
 
  
-    if (tSeason.indexOf("a", 0) > -1) { tSeason = "a"; }
-    else if (tSeason.indexOf("i", 0) > -1) { tSeason = "i"; }
-    else { tSeason = "u"; }
+   // if (tSeason.indexOf("a", 0) > -1) { tSeason = "a"; }
+    //else if (tSeason.indexOf("i", 0) > -1) { tSeason = "i"; }
+   // else { tSeason = "u"; }
 
     //birthplace => First chars ()=> substr(0, 2)
-    grosspay = hourly * hours;
+    //grosspay = hourly * hours;
 
     //Jedi Name = tEmail + tFirst_Name + tSmurf + "-" + tBirthplace + tSeason;
-    let netpay = "net:" + grosspay - taxes ;
+    //let netpay = "net:" + grosspay - taxes ;
 
-    alert("Your Sith-Name henceforth will be: " + netpay);
+    //alert("Your Sith-Name henceforth will be: " + netpay);
 
     // cancel form submit if any user entries are invalid
     if (false=== false) {
         evt.preventDefault();
     }
+    alert(("Employee Name: "+ name + 
+     " Take Home weekly pay: " + netpay));
+
+
+
 
 };
+
 
 
 const clearForm = () => {
 
     //clear each text-box
-    $("#email_1").value = "";
-    $("#birthplace").value = "";
-    $("#season").value = "";
-    $("#smurf").value = "";
-    $("#first_name").value = "";
+
+    $("#hourly").value = "";
+    $("#hours").value = "";
+    $("#tax").value = "";
+    $("#name").value = "";
+    
 
     //clear span elements
-    $("#email_1_error").textContent = "*";
-    $("#birthplace_error").textContent = "*";
-    $("#season_error").textContent = "*";
-    $("#smurf_error").textContent = "*";
-    $("#first_name_error").textContent = "*";
+    $("#hourly").textContent = "*";
+    $("#hours").textContent = "*";
+    $("#tax").textContent = "*";
+    $("#name").textContent = "*";
+ 
     
 
     //set focus for first text-box after resetting the form
-    $("#email_1").focus();
+    $("#hourly").focus();
 };
 
 
@@ -98,6 +112,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //focus for first text-box once the form loads
  
-    $("#email_1").focus();
+    $("#hourly").focus();
     }
 );
